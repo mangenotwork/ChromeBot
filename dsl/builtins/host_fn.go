85661c7028d86e8f4e7abf95b2818a3eebaa26ed
liman <2912882908@qq.com>
2026-03-19 10:32:35 +0800

    feat: 新增网站扫描方法

--- dsl/builtins/host_fn.go
+++ dsl/builtins/host_fn.go
@@ -2,8 +2,13 @@ package builtins
 
 import (
 	"ChromeBot/dsl/interpreter"
+	"bytes"
 	"fmt"
+	"net"
+	"strconv"
 	"strings"
+	"sync"
+	"time"
 
 	gt "github.com/mangenotwork/gathertool"
 )
@@ -14,8 +19,8 @@ var hostFn = map[string]interpreter.Function{
 	"SearchPort":         hostSearchPort,         // SearchPort(ip) 端口扫描方法
 	"WebSiteScanBadLink": hostWebSiteScanBadLink, // WebSiteScanBadLink(domain, depth) 网站死链检查, depth是遍历网站的深度
 	"WebCertificateInfo": hostWebCertificateInfo, // WebCertificateInfo(domain) 网站证书信息
-	"WebScanUrl":         hostWebScanUrl,         // WebScanUrl(domain) NewHostScanUrl 创建扫描站点
-	"WebScanExtLinks":    hostWebScanExtLinks,    // WebScanExtLinks(domain) 创建站点链接采集，只支持get请求
+	"WebScanUrl":         hostWebScanUrl,         // WebScanUrl(domain, depth) NewHostScanUrl 创建扫描站点
+	"WebScanExtLinks":    hostWebScanExtLinks,    // WebScanExtLinks(domain, depth) 创建站点链接采集，只支持get请求
 	"WebPageSpeedCheck":  hostWebPageSpeedCheck,  // WebPageSpeedCheck(domain, depth) NewHostPageSpeedCheck 创建站点所有url测速，只支持get请求
 }
 
@@ -64,9 +69,46 @@ func hostSearchPort(args []interpreter.Value) (interpreter.Value, error) {
 		return nil, fmt.Errorf("SearchPort(ip) 需要一个参数")
 	}
 
-	fmt.Println("SearchPort ....")
+	ip, ipOK := args[0].(string)
+	if !ipOK {
+		return nil, fmt.Errorf("SearchPort(ip)  参数要求是字符串 ")
+	}
 
-	// todo....
+	timeOut := 4 * time.Second
+	queue := gt.NewQueue()
+	for i := 0; i < 65536; i++ {
+		buf := &bytes.Buffer{}
+		buf.WriteString(ip)
+		buf.WriteString(":")
+		buf.WriteString(strconv.Itoa(i))
+		_ = queue.Add(&gt.Task{
+			Url: buf.String(),
+		})
+	}
+
+	var wg sync.WaitGroup
+	for job := 0; job < 65536; job++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			for {
+				if queue.IsEmpty() {
+					break
+				}
+				task := queue.Poll()
+				if task == nil {
+					continue
+				}
+				conn, err := net.DialTimeout("tcp", task.Url, timeOut)
+				if err == nil {
+					fmt.Println(task.Url, "开放")
+					_ = conn.Close()
+				}
+			}
+		}(job)
+	}
+	wg.Wait()
+	fmt.Println("端口扫描完成！！！")
 
 	return nil, nil
 }
@@ -76,10 +118,22 @@ func hostWebSiteScanBadLink(args []interpreter.Value) (interpreter.Value, error)
 		return nil, fmt.Errorf("WebSiteScanBadLink(domain, depth) 需要一个参数")
 	}
 
-	fmt.Println("WebSiteScanBadLink ....")
+	fmt.Println("执行死链检查 ....")
 
-	// todo....
+	domain, domainOK := args[0].(string)
+	if !domainOK {
+		return nil, fmt.Errorf("WebSiteScanBadLink(domain, depth)  参数要求是字符串 ")
+	}
 
+	depth, depthOK := args[1].(int64)
+	if !depthOK {
+		return nil, fmt.Errorf("WebSiteScanBadLink(domain, depth)  depth参数要求是整数 ")
+	}
+
+	obj := gt.NewHostScanBadLink(domain, int(depth))
+	res, count := obj.Run()
+	fmt.Println("死链数量: ", count)
+	fmt.Println("死链: ", strings.Join(res, ";"))
 	return nil, nil
 }
 
@@ -88,22 +142,51 @@ func hostWebCertificateInfo(args []interpreter.Value) (interpreter.Value, error)
 		return nil, fmt.Errorf("WebCertificateInfo(domain) 需要一个参数")
 	}
 
-	fmt.Println("WebCertificateInfo ....")
+	domain, domainOK := args[0].(string)
+	if !domainOK {
+		return nil, fmt.Errorf("WebSiteScanBadLink(domain, depth)  参数要求是字符串 ")
+	}
 
-	// todo....
+	certificateInfo, _ := gt.GetCertificateInfo(domain)
+
+	fmt.Println("证书 url : ", certificateInfo.Url)
+	fmt.Println("证书 有效时间 : ", certificateInfo.EffectiveTime)
+	fmt.Println("证书 起始 : ", certificateInfo.NotBefore)
+	fmt.Println("证书 结束 : ", certificateInfo.NotAfter)
+	fmt.Println("证书 DNSName : ", certificateInfo.DNSName)
+	fmt.Println("证书 OCSPServer : ", certificateInfo.OCSPServer)
+	fmt.Println("证书 CRL分发点 : ", certificateInfo.CRLDistributionPoints)
+	fmt.Println("证书 颁发者 : ", certificateInfo.Issuer)
+	fmt.Println("证书 颁发证书URL : ", certificateInfo.IssuingCertificateURL)
+	fmt.Println("证书 公钥算法 : ", certificateInfo.PublicKeyAlgorithm)
+	fmt.Println("证书 颁发对象 : ", certificateInfo.Subject)
+	fmt.Println("证书 版本 : ", certificateInfo.Version)
+	fmt.Println("证书 证书算法 : ", certificateInfo.SignatureAlgorithm)
 
 	return nil, nil
 }
 
 func hostWebScanUrl(args []interpreter.Value) (interpreter.Value, error) {
-	if len(args) != 1 {
-		return nil, fmt.Errorf("WebScanUrl(domain) 需要一个参数")
+	if len(args) != 2 {
+		return nil, fmt.Errorf("WebScanUrl(domain, depth) 需要一个参数")
 	}
 
-	fmt.Println("WebScanUr ....")
+	fmt.Println("WebScanUrl ....")
 
-	// todo....
+	domain, domainOK := args[0].(string)
+	if !domainOK {
+		return nil, fmt.Errorf("WebScanUrl(domain, depth)  参数要求是字符串 ")
+	}
 
+	depth, depthOK := args[1].(int64)
+	if !depthOK {
+		return nil, fmt.Errorf("WebScanUrl(domain, depth)  depth参数要求是整数 ")
+	}
+
+	obj := gt.NewHostScanUrl(domain, int(depth))
+	res, count := obj.Run()
+	fmt.Println("数量: ", count)
+	fmt.Println("扫描站点的url: ", strings.Join(res, ";"))
 	return nil, nil
 }
 
@@ -114,8 +197,15 @@ func hostWebScanExtLinks(args []interpreter.Value) (interpreter.Value, error) {
 
 	fmt.Println("WebScanExtLinks ....")
 
-	// todo....
+	domain, domainOK := args[0].(string)
+	if !domainOK {
+		return nil, fmt.Errorf("WebScanExtLinks(domain)  参数要求是字符串 ")
+	}
 
+	obj := gt.NewHostScanExtLinks(domain)
+	res, count := obj.Run()
+	fmt.Println("数量: ", count)
+	fmt.Println("站点链接采集: ", strings.Join(res, ";"))
 	return nil, nil
 }
 
@@ -126,7 +216,19 @@ func hostWebPageSpeedCheck(args []interpreter.Value) (interpreter.Value, error) 
 
 	fmt.Println("WebPageSpeedCheck ....")
 
-	// todo....
+	domain, domainOK := args[0].(string)
+	if !domainOK {
+		return nil, fmt.Errorf("WebPageSpeedCheck(domain, depth)  参数要求是字符串 ")
+	}
 
+	depth, depthOK := args[1].(int64)
+	if !depthOK {
+		return nil, fmt.Errorf("WebPageSpeedCheck(domain, depth)  depth参数要求是整数 ")
+	}
+
+	obj := gt.NewHostPageSpeedCheck(domain, int(depth))
+	res, count := obj.Run()
+	fmt.Println("数量: ", count)
+	fmt.Println("站点所有url测速: ", strings.Join(res, ";"))
 	return nil, nil
 }
